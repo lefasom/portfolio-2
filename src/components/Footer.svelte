fe07b605bf7be518e26d5927890c726e3cf71cb4
lefasom1993 <lefasom1993@gmail.com>
2023-12-17 21:20:05 -0300

    ui ux

---
+++ src/components/Footer.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+
+</script>
+
+<header class="bg-gradient-to-l from-slate-700 to-slate-950 flex flex-col items-center" >
+    <h2 class="text-zinc-100 p-5 font-semibold">- Astro, Svelte, Typescript y Tailwind</h2>  
+    <h1 class="text-zinc-100 p-5 font-semibold">- Leonardo Fabián Sombra</h1>
+</header>
--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -2,10 +2,10 @@
 
 </script>
 
-<nav class="fixed bottom-0 left-0 right-0 bg-gradient-to-l from-slate-700 to-slate-950">
+<nav class="fixed z-50 top-0 left-0 right-0 bg-gradient-to-l from-slate-700 to-slate-950">
     <ul class="flex justify-around p-3">
+        <a href="/" class="text-zinc-100 m-2 cursor-pointer hover:text-blue-400">Proyectos</a>
         <li class="text-zinc-100 m-2 cursor-pointer hover:text-blue-400">Github</li>
         <li class="text-zinc-100 m-2  cursor-pointer hover:text-blue-400">Whatsapp</li>
-        <li class="text-zinc-100 m-2  cursor-pointer hover:text-blue-400">Instagram</li>
     </ul>
 </nav>
